fec61011e2944ec03c1bc7ab66442ffe4dbc0aad
Krunal2509 <krunal.sojitra143@gmail.com>
2026-03-19 22:07:53 +0530

    initial commit

---
+++ src/main/java/com/service/bookverse/BookVerseApplication.java
@@ -0,0 +1,14 @@
+package com.service.bookverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookVerseApplication.class, args);
+        System.out.println("Welcome");
+    }
+
+}
